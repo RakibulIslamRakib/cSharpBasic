a0e29d2b030543af09d369e51d9e5edd4a2de1a8
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-10 12:19:09 +0600

    Declare and print all type of primitive variables

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿// bool,char,int,long,double,float, string etc and print them all
+
+int intNumber = 30;
+Console.WriteLine($"Int number is {intNumber}");
+
+string nameString = "Rakibul Islam";
+Console.WriteLine($"The nameString is {nameString}");
+
+bool isStrong = true;
+Console.WriteLine($"boolean value is {intNumber}");
+
+char ch = 'a';
+Console.WriteLine($"The char is {ch}");
+
+long longNumber = 2000000000;
+Console.WriteLine($"The longNumber is {longNumber}");
+
+double doubleNumber = 45.34545;
+Console.WriteLine($"The double number  is {doubleNumber}");
+
+float floatNumber = 34;
+Console.WriteLine($"The floatNumber is {floatNumber}");
