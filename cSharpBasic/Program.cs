450a25a4f97d14b0d427dfe08645e9d2f823c8dc
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-10 18:34:54 +0600

    class and object created

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,40 @@
 ﻿
+namespace cSharpBasic
+{
+
+    class Student
+    {
+        public string name;
+        public int age;
+
+        public Student(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void printName()
+        {
+            Console.WriteLine("name is : " + name);
+        }
+
+       public  void printAge()
+        {
+            Console.WriteLine("Age is : " + age);
+        }
+
+        static string faculty = "CSE";
+
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student("Rakibul Islam",25);
+
+            student1.printName();
+            student1.printAge();
+        }
+    }
+
+}
