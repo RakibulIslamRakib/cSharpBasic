4d138e5ecd67fb29f90c0a726d311412977d8a76
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-10 18:44:05 +0600

    example of structure added

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿struct Book
+{
+    public string name;
+    public string description;
+    public int prize;
+
+    public void print()
+    {
+        System.Console.WriteLine("name is " + name);
+        System.Console.WriteLine("Description : " + description);
+        System.Console.WriteLine("Price : "+ prize);
+    }
+}
+
+public class Test
+{
+    static void Main(string[] args)
+    {
+        var book1 = new Book();
+        book1.name = "Hajar bochor dhore";
+        book1.description = "Hajar bochor dhore is a novel based on village life";
+        book1.prize = 350;
+
+        book1.print();
+
+
+        var book2 = new Book();
+        book2.name = "kobor";
+        book2.description = "obor is a poem based on village life";
+        book2.prize = 380;
+
+        book2.print();
+    }
+}
