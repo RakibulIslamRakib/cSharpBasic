78293c095f4ea02b4b8c002a62238bfb9345589d
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-10 16:44:53 +0600

    operators operations added

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿
+//(+,-,*,/,%, |,^,&)
+
+Console.WriteLine($"3+4 = {3+4}");
+Console.WriteLine($"7-4 = {7-4}");
+Console.WriteLine($"7*4 = {7 * 4}");
+Console.WriteLine($"20 / 4 = {20/4}");
+Console.WriteLine($"30%4 = {30%4}");
+Console.WriteLine($"7^4 = {7 ^ 4}");
+Console.WriteLine($"7|4 = {7 | 4}");
+Console.WriteLine($"7 & 4 = {7 & 4}");
