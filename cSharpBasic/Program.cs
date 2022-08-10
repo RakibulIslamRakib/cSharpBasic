cd273e31b605754f6e3d5a1596edd5463e1bb823
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-10 17:04:01 +0600

    example of if else and ternary operations added

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿int x = 5, y = 3, z = 8;
+
+int max = x;
+if (x >z && x>y)
+{
+    max = x;
+}
+else if (y> x && y > z)
+{
+    max = y;
+}
+
+else
+{
+    max = z;
+}
+
+Console.WriteLine($"max among {x} , {y}, {z} = {max}");
+
+
+//ternary
+int maxAmonThree(int x,int y,int z)
+{
+   return  x > y ? x > z ? x :z :y > z? y : z;
+}
+
+Console.WriteLine( maxAmonThree(3, 8, 5));
