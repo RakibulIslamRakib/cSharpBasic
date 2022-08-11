0b783b550060b56910fb1e44863cb463bfa4260a
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-11 12:03:30 +0600

    static method example added successfully

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿
+
+namespace StaticExample
+{
+    class Demo1
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello from Demo1 class method !");
+        }
+
+    }
+    class Demo2
+    {
+        public static void sayHello()
+        {
+            Console.WriteLine("Hello from Demo2 class static method !");
+        }
+
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //calling norlam method using object
+            var demo1 = new Demo1();
+            Demo2.sayHello();
+
+            //calling norlam method using object
+            var demo2 = new Demo2();
+
+            /*
+            demo2.sayHello();  error occurs. sayHello cannot be acces from object
+            */
+
+            Demo2.sayHello();
+            /*
+             static method should call using classname.staticmethod name
+            because static method   are referenced by the class name itself 
+            */
+        }
+    }
+}
