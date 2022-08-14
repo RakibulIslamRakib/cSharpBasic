964d9e8f31e9a7e39660374f52264f52b1b15ac2
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-14 11:59:44 +0600

    enum example added

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿
+
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{Planets.Earch} is at {(int)Planets.Earch}");
+        Console.WriteLine($"{Planets.Earch.ToString()} is at {(int)Planets.Earch}");
+
+
+        Planets day1,day2;int i = 1;
+
+        day1 = (Planets)i;
+        day2 = (Planets)2;
+        Console.WriteLine($"second planet is {day2}");
+
+        for(int i2 = 1; i2 < 8; i2++)
+        {
+            Console.WriteLine($"{i2} th planet is : {(Planets)i2}");
+        }
+
+    }
+}
+
+enum Planets
+{
+    Mercury=1,
+    Venus,
+    Earch,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune,
+    Pluto
+
+}
