6cf6ef0ef673eeb8df09fb1bcca90aa0c8fb7d5e
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-14 11:46:53 +0600

    datetime example on the way

--- cSharpBasic/Program.cs
+++ cSharpBasic/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, from main branch");
+﻿var date1 = new DateTime(2015,12,25);
+Console.WriteLine(date1.ToString());//// 12/25/2015 12:00:00 AM
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.MinValue);
+Console.WriteLine(DateTime.MaxValue);
+Console.WriteLine(DateTime.UtcNow);
