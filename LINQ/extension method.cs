9be849c0e2f2792f525dfa56a99ca391a779f9b8
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2022-08-26 19:03:44 +0600

    Create extension method.cs
    extension methods is  static and contain this keyword in parameter

---
+++ LINQ/extension method.cs
@@ -0,0 +1,29 @@
+namespace demo
+{
+
+  public class StringHelper
+  {
+      public static string ChangeFirstLatterCase(this string inputString)
+      {
+
+      return newmodifiedString;
+
+      }
+
+  }
+
+}
+
+
+namespace demo
+{
+
+  public class Progran
+  {
+      string strName = "pragim";
+      string result  = strName.ChangeFirstLatterCase(); // this is called the extension methods. its static and contain this keyword in parameter
+      cw(result);
+
+  }
+
+}
