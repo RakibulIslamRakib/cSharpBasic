d7cd2de708b06f0ab9ae4cc3434f005c98959fa2
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2022-08-29 12:50:37 +0600

    Create dependency_Injection.cs

---
+++ dependency_Injection.cs
@@ -0,0 +1,113 @@
+public class MyDependency
+{
+    public void WriteMessage(string message)
+    {
+        Console.WriteLine($"MyDependency.WriteMessage called. Message: {message}");
+    }
+}
+
+
+
+
+public class IndexModel : PageModel
+{
+    private readonly MyDependency _dependency = new MyDependency();
+
+    public void OnGet()
+    {
+        _dependency.WriteMessage("IndexModel.OnGet");
+    }
+}
+public class IndexModel : PageModel
+{
+    private readonly MyDependency _dependency = new MyDependency();
+
+    public void OnGet()
+    {
+        _dependency.WriteMessage("IndexModel.OnGet");
+    }
+}
+
+
+
+solve dependency issue;
+
+
+
+
+public interface IMyDependency
+{
+    void WriteMessage(string message);
+}
+
+
+
+public class MyDependency : IMyDependency
+{
+    public void WriteMessage(string message)
+    {
+        Console.WriteLine($"MyDependency.WriteMessage Message: {message}");
+    }
+}
+
+
+
+using DependencyInjectionSample.Interfaces;
+using DependencyInjectionSample.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<IMyDependency, MyDependency>();
+
+var app = builder.Build();
+
+
+
+//In the sample app, the IMyDependency service is requested and used to call the WriteMessage method:
+public class Index2Model : PageModel
+{
+    private readonly IMyDependency _myDependency;
+
+    public Index2Model(IMyDependency myDependency)
+    {
+        _myDependency = myDependency;            
+    }
+
+    public void OnGet()
+    {
+        _myDependency.WriteMessage("Index2Model.OnGet");
+    }
+}
+
+
+
+The implementation of the IMyDependency interface can be improved by using the built-in logging API:
+public class MyDependency2 : IMyDependency
+{
+    private readonly ILogger<MyDependency2> _logger;
+
+    public MyDependency2(ILogger<MyDependency2> logger)
+    {
+        _logger = logger;
+    }
+
+    public void WriteMessage(string message)
+    {
+        _logger.LogInformation( $"MyDependency2.WriteMessage Message: {message}");
+    }
+}
+
+program.cs
+using DependencyInjectionSample.Interfaces;
+using DependencyInjectionSample.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<IMyDependency, MyDependency2>();
+
+var app = builder.Build();
+MyDependency2 depends on ILog
